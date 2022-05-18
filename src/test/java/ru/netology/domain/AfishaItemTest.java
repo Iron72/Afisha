1419af0ce18c221dd532d54b6d96574f429781c9
iron72 <anatoliy-elkin@mail.ru>
2022-05-18 17:37:55 +0300

    Add tests

--- src/test/java/ru/netology/domain/AfishaItemTest.java
+++ src/test/java/ru/netology/domain/AfishaItemTest.java
@@ -1,7 +1,22 @@
 package ru.netology.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class AfishaItemTest {
 
+    @Test
+    void getFilmId() {
+    }
+
+    @Test
+    void getFilmName() {
+    }
+
+    @Test
+    void setFilmId() {
+    }
+
+    @Test
+    void setFilmName() {
+    }
 }
